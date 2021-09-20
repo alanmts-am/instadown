411d309071b3bd6105a8c70865c52e6b55295b2f
alan-playg <alanmssilva@outlook.com>
2021-09-19 21:14:39 -0300

    Add option to informe the profiles file and download dir path

--- src/__main__.py
+++ src/__main__.py
@@ -9,9 +9,6 @@ from utils.create import create_directory
 from utils.extract import extract_data
 from utils.move import move_dirs
 
-PROFILES_FILE = 'profiles.txt'
-FILE_DIR = 'files'
-
 def download_all_posts_from_file(profiles_file, target_dir):
     with open(profiles_file, 'r') as file:
         line_itens = file.readlines()
@@ -63,6 +60,8 @@ def main():
     parser.add_argument('--idate', action='store', dest='initial_date', default=None, help='Insira a data inicial dos downloads')
     parser.add_argument('--fdate', action='store', dest='final_date', default=None, help='Insira a data final dos downloads')
     parser.add_argument('--profile', action='store', dest='profile', default=None, help='Insira user específico do download')
+    parser.add_argument('--profile-file', action='store', dest='profile_file', default='profiles.txt', help='Insira o caminho e tipo do arquivo de profiles')
+    parser.add_argument('--download-dir', action='store', dest='download_dir', default='files', help='Insira o caminho da pasta de download padrão')
     args = parser.parse_args()
 
     if (not args.post and not args.story):
@@ -73,17 +72,17 @@ def main():
     if args.post and not args.story: # post section
         if args.profile is None: # No profile, just from file
             if (args.initial_date is None and args.final_date is None):
-                download_all_posts_from_file(PROFILES_FILE, FILE_DIR)
+                download_all_posts_from_file(args.profile_file, args.download_dir)
 
             elif (args.initial_date != None and args.final_date != None):
-                download_post_by_date_from_file(PROFILES_FILE, args.initial_date, args.final_date, FILE_DIR)
+                download_post_by_date_from_file(args.profile_file, args.initial_date, args.final_date, args.download_dir)
 
         elif args.profile != None: # profile found
             if (args.initial_date is None and args.final_date is None):
-                download_all_posts_from_profile(args.profile, FILE_DIR)
+                download_all_posts_from_profile(args.profile, args.download_dir)
 
             elif (args.initial_date != None and args.final_date != None):
-                download_post_by_date_from_profile(args.profile, args.initial_date, args.final_date, FILE_DIR)
+                download_post_by_date_from_profile(args.profile, args.initial_date, args.final_date, args.download_dir)
 
 if __name__ == '__main__':
     main()
