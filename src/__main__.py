8db36feaded74be6b1b208960d7ef24daebb5239
alan-playg <alanmssilva@outlook.com>
2021-09-25 21:43:20 -0300

    Implements stories download

--- src/__main__.py
+++ src/__main__.py
@@ -3,6 +3,7 @@ import sys
 from datetime import datetime
 
 from src.posts import get_all_posts, get_data_posts
+from src.stories import get_stories
 from utils.create import create_directory
 from utils.extract import extract_data
 from utils.move import move_dirs
@@ -49,12 +50,29 @@ def download_post_by_date_from_profile(profiles_name, initial_date, final_date, 
     get_data_posts(profiles_name, profiles_name, SINCE, UNTIL)
     move_dirs(profiles_name, target_dir)
 
+def download_story_from_file(user, pwsrd, profiles_file, target_dir):
+    with open(profiles_file, 'r') as file:
+        line_itens = file.readlines()
+    
+    for line_item in line_itens:
+        line_item = line_item.replace('\n', '')
+        create_directory(line_item)
+        create_directory(target_dir)
+        get_stories(user, pwsrd, line_item)
+        move_dirs(line_item, target_dir)
+
+def download_story_from_profile(user, pwsrd, profile_name, target_dir):
+    create_directory(profile_name)
+    create_directory(target_dir)
+    get_stories(user, pwsrd, profile_name)
+    move_dirs(profile_name, target_dir)
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--post', action='store_true', dest='post', default=None, help='Indicativo de download dos posts')
     parser.add_argument('--story', action='store_true', dest='story', default=None, help='Indicativo de download dos stories')
-    parser.add_argument('--user', action='store', dest='username', default=None, help='Insira o seu usuário')
-    parser.add_argument('--pass', action='store', dest='password', default=None, help='Insira a sua senha')
+    parser.add_argument('--username', action='store', dest='username', default=None, help='Insira o seu usuário')
+    parser.add_argument('--password', action='store', dest='password', default=None, help='Insira a sua senha')
     parser.add_argument('--idate', action='store', dest='initial_date', default=None, help='Insira a data inicial dos downloads')
     parser.add_argument('--fdate', action='store', dest='final_date', default=None, help='Insira a data final dos downloads')
     parser.add_argument('--profile', action='store', dest='profile', default=None, help='Insira user específico do download')
@@ -81,6 +99,14 @@ def main():
 
             elif (args.initial_date != None and args.final_date != None):
                 download_post_by_date_from_profile(args.profile, args.initial_date, args.final_date, args.download_dir)
+
+    if not args.post and args.story: # story section
+        if args.profile is None: # No profile, just from file
+            if(args.username != None or args.password != None):
+                download_story_from_file(args.username, args.password, args.profile_file, args.download_dir)
+        elif args.profile != None:
+            if(args.username != None or args.password != None):
+                download_story_from_profile(args.username, args.password, args.profile, args.download_dir)
 
 if __name__ == '__main__':
     main()
