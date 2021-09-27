e93cd33ca0046458240b187b859f44ef14620cc7
alan-playg <alanmssilva@outlook.com>
2021-09-26 22:29:04 -0300

    Fix user not found error with try except

--- src/posts.py
+++ src/posts.py
@@ -3,31 +3,39 @@ import instaloader
 from utils.extract import extract_posts_only
 
 def get_data_posts(user, folder, since, until):
-    L = instaloader.Instaloader()
-    posts = instaloader.Profile.from_username(L.context, user).get_posts()
+    try:
+        L = instaloader.Instaloader()
+        posts = instaloader.Profile.from_username(L.context, user).get_posts()
 
-    for post in posts:
-        if (post.date >= since) and (post.date <= until):
+        for post in posts:
+            if (post.date >= since) and (post.date <= until):
+                print("Baixando... ")
+                L.download_post(post, user)
+                extract_posts_only(folder, user)
+                print("\nArquivos até aqui de " + user + ": " + str(count_posts(folder)) + "\n")
+            elif post.date < since: 
+                break
+            
+        print("\nArquivos totais de " + user + ": " + str(count_posts(folder)) + "\n")
+    except:
+        print("Usuário " + user + " não encontrado")
+        pass
+
+def get_all_posts(user, folder):
+    try:
+        L = instaloader.Instaloader()
+        posts = instaloader.Profile.from_username(L.context, user).get_posts()
+
+        for post in posts:
             print("Baixando... ")
             L.download_post(post, user)
             extract_posts_only(folder, user)
             print("\nArquivos até aqui de " + user + ": " + str(count_posts(folder)) + "\n")
-        elif post.date < since: 
-            break
-    
-    print("\nArquivos totais de " + user + ": " + str(count_posts(folder)) + "\n")
 
-def get_all_posts(user, folder):
-    L = instaloader.Instaloader()
-    posts = instaloader.Profile.from_username(L.context, user).get_posts()
-
-    for post in posts:
-        print("Baixando... ")
-        L.download_post(post, user)
-        extract_posts_only(folder, user)
-        print("\nArquivos até aqui de " + user + ": " + str(count_posts(folder)) + "\n")
-    
-    print("\nArquivos totais de " + user + ": " + str(count_posts(folder)) + "\n")
+        print("\nArquivos totais de " + user + ": " + str(count_posts(folder)) + "\n")
+    except:
+        print("Usuário " + user + " não encontrado")
+        pass
 
 def count_posts(folder):
     x = 0
--- src/stories.py
+++ src/stories.py
@@ -2,9 +2,13 @@ from utils.extract import extract_posts_only
 import instaloader
 
 def get_stories(user, password, profile_name):
-    L = instaloader.Instaloader()
-    L.login(user, password)
-    profile = instaloader.Profile.from_username(L.context, username=profile_name)
-    L.download_stories(userids=[profile.userid], filename_target=profile_name)
+    try:
+        L = instaloader.Instaloader()
+        L.login(user, password)
+        profile = instaloader.Profile.from_username(L.context, username=profile_name)
+        L.download_stories(userids=[profile.userid], filename_target=profile_name)
 
-    extract_posts_only(profile_name, profile_name)
+        extract_posts_only(profile_name, profile_name)
+    except:
+        print("Usuário " + user + " não encontrado")
+        pass
