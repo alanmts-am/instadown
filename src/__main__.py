76b4070e6786cb2bad6f11068e6afa754536cdf8
alanmts-am <alanmts.am@gmail.com>
2023-09-29 21:25:16 -0300

    refactor: remove story argument

--- src/__main__.py
+++ src/__main__.py
@@ -8,8 +8,6 @@ from utils.extract import extract_data, increase_data_days
 from utils.move import move_dirs
 
 from src.posts import get_all_posts, get_data_posts
-from src.stories import get_stories
-
 
 def download_all_posts_from_file(profiles_file, target_dir):
     with open(profiles_file, 'r') as file:
@@ -48,27 +46,9 @@ def download_post_by_date_from_profile(profiles_name, initial_date, final_date, 
     os.chdir(target_dir)
     get_data_posts(profiles_name, SINCE, UNTIL)
 
-def download_story_from_file(user, pwsrd, profiles_file, target_dir):
-    with open(profiles_file, 'r') as file:
-        line_itens = file.readlines()
-    
-    create_directory(target_dir)
-    os.chdir(target_dir)
-    for line_item in line_itens:
-        line_item = line_item.replace('\n', '')
-        get_stories(user, pwsrd, line_item)
-
-def download_story_from_profile(user, pwsrd, profile_name, target_dir):
-    create_directory(target_dir)
-    os.chdir(target_dir)
-    get_stories(user, pwsrd, profile_name)
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--post', action='store_true', dest='post', default=None, help='Indicativo de download dos posts')
-    parser.add_argument('--story', action='store_true', dest='story', default=None, help='Indicativo de download dos stories')
-    parser.add_argument('--username', action='store', dest='username', default=None, help='Insira o seu usuário')
-    parser.add_argument('--password', action='store', dest='password', default=None, help='Insira a sua senha')
     parser.add_argument('--idate', action='store', dest='initial_date', default=None, help='Insira a data inicial dos downloads')
     parser.add_argument('--fdate', action='store', dest='final_date', default=None, help='Insira a data final dos downloads')
     parser.add_argument('--today', action='store_true', dest='today', default=None, help='Indicativo de data atual para o idate e fdate')
@@ -77,11 +57,11 @@ def main():
     parser.add_argument('--download-dir', action='store', dest='download_dir', default='files', help='Insira o caminho da pasta de download padrão')
     args = parser.parse_args()
 
-    if (not args.post and not args.story):
+    if (not args.post):
         print('É necessário pelo menos a indicação de um dos comandos --post ou --story\n\nUtilize o argumento --help para verificar a forma correta')
         sys.exit()
 
-    elif args.post and not args.story: # post section
+    elif args.post: # post section
         if args.profile is None: # No profile, just from file
             if(args.today and (args.initial_date is None and args.final_date is None)):
                 today = datetime.today().strftime('%d/%m/%Y')
@@ -112,13 +92,5 @@ def main():
                 print('Argumentos utilizados incorretamente\n\nUtilize o argumento --help para verificar a forma correta')
                 sys.exit()
 
-    elif not args.post and args.story: # story section
-        if args.profile is None: # No profile, just from file
-            if(args.username != None or args.password != None):
-                download_story_from_file(args.username, args.password, args.profile_file, args.download_dir)
-        elif args.profile != None:
-            if(args.username != None or args.password != None):
-                download_story_from_profile(args.username, args.password, args.profile, args.download_dir)
-    
 if __name__ == '__main__':
     main()
