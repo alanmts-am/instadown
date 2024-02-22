e3c462fba524b16d9bab19895573d64d1a2ec0a7
alanmts-am <alanmts.am@gmail.com>
2024-02-21 21:00:47 -0300

    refactor: change profile file txt to json

--- src/__main__.py
+++ src/__main__.py
@@ -2,6 +2,7 @@ import argparse
 from datetime import datetime
 import os
 import sys
+import json
 
 from utils.create import create_directory
 from utils.extract import extract_data, increase_data_days
@@ -9,34 +10,32 @@ from utils.move import move_dirs
 
 from src.posts import get_all_posts, get_data_posts
 
-def download_all_posts_from_file(profiles_file, target_dir):
-    with open(profiles_file, 'r') as file:
-        line_itens = file.readlines()
+def download_all_posts_from_file(profiles_json, target_dir):
+    with open(profiles_json, 'r') as profiles:
+        profiles = json.load(profiles)
     
     create_directory(target_dir)
     os.chdir(target_dir)
-    for line_item in line_itens:
-        line_item = line_item.replace('\n', '')
-        get_all_posts(line_item, line_item)
-        move_dirs(line_item, target_dir)
+    for profile in profiles['profiles']:
+        get_all_posts(profile)
+        move_dirs(profile, target_dir)
 
 def download_all_posts_from_profile(profile, target_dir):
     create_directory(target_dir)
     os.chdir(target_dir)
     get_all_posts(profile)
 
-def download_post_by_date_from_file(profiles_file, initial_date, final_date, target_dir):
+def download_post_by_date_from_file(profiles_json, initial_date, final_date, target_dir):
     SINCE = extract_data(initial_date)
     UNTIL = increase_data_days(final_date, 1)
     
-    with open(profiles_file, 'r') as file:
-        line_itens = file.readlines()
+    with open(profiles_json, 'r') as profiles:
+        profiles = json.load(profiles)
         
     create_directory(target_dir)
     os.chdir(target_dir)
-    for line_item in line_itens:
-        line_item = line_item.replace('\n', '')
-        get_data_posts(line_item, SINCE, UNTIL)
+    for profile in profiles['profiles']:
+        get_data_posts(profile, SINCE, UNTIL)
 
 def download_post_by_date_from_profile(profiles_name, initial_date, final_date, target_dir):
     SINCE = extract_data(initial_date)
@@ -53,18 +52,18 @@ def main():
     parser.add_argument('--fdate', action='store', dest='final_date', default=None, help='Insira a data final dos downloads')
     parser.add_argument('--today', action='store_true', dest='today', default=None, help='Indicativo de data atual para o idate e fdate')
     parser.add_argument('--profile', action='store', dest='profile', default=None, help='Insira user específico do download')
-    parser.add_argument('--profile-file', action='store', dest='profile_file', default='profiles.txt', help='Insira o caminho e tipo do arquivo de profiles')
-    parser.add_argument('--download-dir', action='store', dest='download_dir', default='files', help='Insira o caminho da pasta de download padrão')
+    parser.add_argument('--file', action='store', dest='profile_file', default='profiles.json', help='Insira o caminho do arquivo JSON de profiles')
+    parser.add_argument('--target', action='store', dest='download_dir', default='profiles', help='Insira o caminho da pasta de download padrão')
     args = parser.parse_args()
 
     if (not args.post):
-        print('É necessário pelo menos a indicação de um dos comandos --post ou --story\n\nUtilize o argumento --help para verificar a forma correta')
+        print('É necessário a indicação do comando --post\n\nUtilize o argumento --help para verificar a forma correta')
         sys.exit()
 
     elif args.post: # post section
         if args.profile is None: # No profile, just from file
             if(args.today and (args.initial_date is None and args.final_date is None)):
-                today = datetime.today().strftime('%d/%m/%Y')
+                today = datetime.today().strftime('%Y-%m-%d')
                 download_post_by_date_from_file(args.profile_file, today, today, args.download_dir)
             
             elif (args.initial_date is None and args.final_date is None):
@@ -79,7 +78,7 @@ def main():
 
         elif args.profile != None: # profile found
             if(args.today and (args.initial_date is None and args.final_date is None)):
-                today = datetime.today().strftime('%d/%m/%Y')
+                today = datetime.today().strftime('%Y-%m-%d')
                 download_post_by_date_from_profile(args.profile, today, today, args.download_dir)
 
             elif (args.initial_date is None and args.final_date is None):
