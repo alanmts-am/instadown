a44c5553bf16053ece4a2b8e31bd35704348163d
alanmts-am <alanmts.am@gmail.com>
2024-02-21 21:03:17 -0300

    refactor: update extract post function to the rigth path

--- utils/extract.py
+++ utils/extract.py
@@ -8,9 +8,10 @@ def extract_data(data):
 def increase_data_days(data, days_):
     return dtclass.strptime(data, '%Y-%m-%d') + dtpack.timedelta(days = days_)
 
-def extract_posts_only():
+def extract_posts_only(profile_dir):
+    os.chdir(profile_dir)
     items = os.listdir()
     for item in items:
         if (item.endswith(".xz") or item.endswith(".txt")):
-            print("Apagando... " + item)
             os.remove(item)
+    os.chdir("../")
