a59e764abca849bb89e43df08461d8788edf80f3
alanmts-am <alanmts.am@gmail.com>
2024-02-21 21:01:12 -0300

    refactor: update terminal return

--- src/posts.py
+++ src/posts.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 import instaloader
 from utils.extract import extract_posts_only
@@ -6,34 +7,32 @@ from utils.extract import extract_posts_only
 
 def get_data_posts(user, since, until):
     try:
+        print(f'''[{datetime.now()}] Buscando por {user}...''')
         L = instaloader.Instaloader()
         posts = instaloader.Profile.from_username(L.context, user).get_posts()
         
+        print(f'''[{datetime.now()}] Baixando...''')
         for post in posts:
             if (post.date >= since) and (post.date <= until):
-                print("Baixando... ")
                 L.download_post(post, user)
             elif post.date < since: 
                 break
             
-        os.chdir(user)
-        extract_posts_only()
-        print("\nArquivos totais de " + user + ": " + str(count_posts()) + "\n")
+        extract_posts_only(user)
     except:
         print("Usuário " + user + " não encontrado ou não contem posts para data informada")
         pass
 
 def get_all_posts(user):
     try:
+        print(f'''[{datetime.now}] Buscando por {user}...''')
         L = instaloader.Instaloader()
         posts = instaloader.Profile.from_username(L.context, user).get_posts()
-        for post in posts:
-            print("Baixando... ")
-            L.download_post(post, '.')
-            extract_posts_only()
-            print("\nArquivos até aqui de " + user + ": " + str(count_posts()) + "\n")
 
-        print("\nArquivos totais de " + user + ": " + str(count_posts()) + "\n")
+        print(f'''[{datetime.now}] Baixando...''')
+        for post in posts:
+            L.download_post(post, '.')
+            extract_posts_only(user)
     except:
         print("Usuário " + user + " não encontrado ou não contem posts para data informada")
         pass
